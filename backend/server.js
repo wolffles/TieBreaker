e26e16473bd691a1c7f4db2a5e80b32b956e726a
Wolffles <wolfgang.truong@gmail.com>
2020-06-11 17:39:44 -0700

    created playerArea and socketUtility components

--- backend/server.js
+++ backend/server.js
@@ -12,19 +12,44 @@ server.listen(port, () => {
 
 
 //Dashboard
-// let numUsers = 0;
-// let players = [];
+let numUsers = 0;
+let players = [];
 
 // io.sockets.on('connect', function(socket) {
 //   const sessionID = socket.id;
 // });
 
-io.on('connection', (client) => {
-    client.on('message', (data) => {
+io.on('connection', (socket) => {
+    socket.join('game1')
+        let addedUser = false
+    socket.on('message', (data) => {
         console.log(data);
-        client.broadcast.emit('message', data);
+        socket.broadcast.emit('message', data);
 
       });
-
+    socket.on('add user', (data) => {
+        if (addedUser) return;
+        if(players.indexOf(data.username) == -1){
+            socket.username = data.username;
+        }else if (players.indexOf(data.username) == -1){
+            socket.username = data.username;
+        }else{
+            socket.username = data.username+'_'
+        }
+        ++numUsers;
+        if(players.indexOf(data.username) == -1){
+            players.push(socket.username);
+            addedUser = true;
+        }
+        socket.emit('login', {
+            numUsers: numUsers
+        });
+        socket.broadcast.emit('user joined', {
+            username: socket.username,
+            numUsers: numUsers,
+            id:data.id,
+            reconnecting:reconnecting
+        });
+    })
       
 });
