623e68ec6f1b688407ddc9eeb992dfea8acd805b
FelixAlvarado <felixalvarado653@gmail.com>
2020-07-29 16:35:05 -0700

    modified server

--- frontend/src/utility/socket.js
+++ frontend/src/utility/socket.js
@@ -2,6 +2,7 @@ import openSocket from 'socket.io-client';
 
 let host = window.origin.includes("herokuapp") ? "https://tie-breaker7.herokuapp.com/" : "http://localhost:3001"
 
+console.log('here is the host server', host);
 export const  socket = openSocket(host);
 
 // to server
